fac12d31fec47ee19958d8d052bd621ce11ae704
korakot <118332936+Korakot2001@users.noreply.github.com>
2023-03-06 11:13:02 +0700

    Create 11.2.cpp

---
+++ Practise1/Lap11/11.2.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void mergesort( int t[], int k ) ;
+void merge( int *u, int m, int *v, int n, int *t ) ;
+
+int main() {
+    int data[ 7 ] = { 4, 6, 1, 2, 5, 1, 8 } ;
+    int n = 7 ;
+    mergesort( data, n ) ;
+    for( int i = 0 ; i < n ; i++ ) printf( "%d ", data[ i ] ) ;
+    return 0 ;
+}//end function
+
+void mergesort( int t[], int k ) {
+    if( k > 1 ) {
+        int m = k / 2;
+        int u[ m ] ;
+        int v[ k - m ] ;
+        for( int i = 0 ; i < m ; i++ ){
+            u[ i ] = t[ i ] ;
+        } //end loop
+        for( int i = 0 ; i < k - m ; i++ ){
+            v[ i ] = t[ m + i ] ;
+        }//end loop
+        mergesort( u, m ) ;
+        mergesort( v, k - m ) ;
+        merge( u, m, v, k - m, t ) ;
+    }
+}//end function
+
+void merge( int *u, int m, int *v, int n, int *t ) {
+    int i = 0, j = 0, k = 0 ;
+    while( i < m && j < n ) {
+        if( u[ i ] < v[ j ] ) t[ k++ ] = u[ i++ ] ;
+        else t[ k++ ] = v[ j++ ] ;
+    }//end loop
+    while( i < m ) {
+        t[ k++ ] = u[ i++ ] ;
+    }//end loop
+    while( j < n ) {
+        t[ k++ ] = v[ j++ ] ;
+    }//end loop
+}//end function
